f15c361ac69e005bec12d8b1cb948eb1827015f1
StarpiaForge <starpia.forge@gmail.com>
2025-02-13 11:14:17 +0900

    [Add] Client.newRequest Method

--- client.go
+++ client.go
@@ -1,5 +1,11 @@
 package clova
 
+import (
+	"context"
+	"io"
+	"net/http"
+)
+
 type Client struct {
 	config ClientConfig
 }
@@ -13,4 +19,61 @@ func NewClientWithConfig(config ClientConfig) *Client {
 	return &Client{
 		config: config,
 	}
+}
+
+type requestOptions struct {
+	body   any
+	header http.Header
+}
+
+type requestOption func(*requestOptions)
+
+func withBody(body any) requestOption {
+	return func(args *requestOptions) {
+		args.body = body
+	}
+}
+
+func withContentType(contentType string) requestOption {
+	return func(args *requestOptions) {
+		args.header.Set("Content-Type", contentType)
+	}
+}
+
+func withAccept(accept string) requestOption {
+	return func(args *requestOptions) {
+		args.header.Set("Accept", accept)
+	}
+}
+
+func (c *Client) newRequest(
+	ctx context.Context,
+	method, url string,
+	opts ...requestOption,
+) (*http.Request, error) {
+	// Default Options
+	args := &requestOptions{
+		body:   nil,
+		header: make(http.Header),
+	}
+
+	for _, opt := range opts {
+		opt(args)
+	}
+
+	var bodyReader io.Reader
+	if args.body != nil {
+		bodyReader = args.body.(io.Reader)
+	}
+
+	req, err := http.NewRequestWithContext(ctx, method, url, bodyReader)
+	if err != nil {
+		return nil, err
+	}
+
+	if args.header != nil {
+		req.Header = args.header
+	}
+
+	return req, nil
 }
